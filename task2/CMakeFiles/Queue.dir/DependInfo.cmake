b869d8ef5d3921f888737c3ea1ac2e732e600e90
wbj820 <2749347286@qq.com>
2021-07-22 14:35:38 +0800

    cmake

---
+++ task2/CMakeFiles/Queue.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wbj820/2021Unique-lab-summer/2021-UniqueLab-SummerCamp/task2/priority_queue.cpp" "/home/wbj820/2021Unique-lab-summer/2021-UniqueLab-SummerCamp/task2/CMakeFiles/Queue.dir/priority_queue.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ task2/CMakeFiles/Queue.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Queue.dir/priority_queue.cpp.o"
+  "Queue"
+  "Queue.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Queue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
